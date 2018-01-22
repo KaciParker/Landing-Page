d8f8487040bb5b3d9c82d7ad6f9273ce674a2afe
Kaci Parker <kaci.joy@hotmail.com>
2018-01-21 23:02:46 -0700

    Fixed scrolling, added in profile logos. Mobile Friendly.

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 var percentage = -.5;
 function myFunction() {
     var scrollers = document.getElementsByClassName("tscroll");
 
-    for(var i = 0; i < scrollers.length; i++) {
-        y = (document.documentElement.scrollTop * percentage) + (i * 100);
-        scrollers[i].style.backgroundPosition="0px " + y + "px";
+    for (var i = 0; i < scrollers.length; i++) {
+        var y = 0;
+        switch (i) {
+            case 0:
+                y = (document.documentElement.scrollTop * percentage) + (i * 100);
+                break;
+            case 1:
+                y = (document.documentElement.scrollTop * percentage) + (i * 600);
+                break;
+            case 2:
+                console.log("here")
+                y = (document.documentElement.scrollTop * percentage) + (i * 555);
+                break;
+            case 3:
+                y = (document.documentElement.scrollTop * percentage) + (i * 850);
+                break;
+        }
+        scrollers[i].style.backgroundPosition = "0px " + y + "px";
     }
 }
