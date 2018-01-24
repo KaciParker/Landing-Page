a73e0bec6f6f754458af405bb52ca4ca51ff3166
Kaci Parker <kaci.joy@hotmail.com>
2018-01-23 23:12:58 -0700

    Fixed scrolling issues. Added in headshot. Fixed Hover links.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onscroll = function () { myFunction() };
 var percentage = -.5;
+var screenWidthBreak = 999;
 function myFunction() {
     var scrollers = document.getElementsByClassName("tscroll");
 
@@ -7,17 +8,32 @@ function myFunction() {
         var y = 0;
         switch (i) {
             case 0:
-                y = (document.documentElement.scrollTop * percentage) + (i * 100);
+                if (window.innerWidth <= screenWidthBreak) {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 100);
+                } else {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 100);
+                }
                 break;
             case 1:
-                y = (document.documentElement.scrollTop * percentage) + (i * 600);
+                if (window.innerWidth <= screenWidthBreak) {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 600);
+                } else {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 600);
+                }
                 break;
             case 2:
-                console.log("here")
-                y = (document.documentElement.scrollTop * percentage) + (i * 555);
+                if (window.innerWidth <= screenWidthBreak) {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 710);
+                } else {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 625);
+                }
                 break;
             case 3:
-                y = (document.documentElement.scrollTop * percentage) + (i * 850);
+                if (window.innerWidth <= screenWidthBreak) {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 1000);
+                } else {
+                    y = (document.documentElement.scrollTop * percentage) + (i * 920);
+                }
                 break;
         }
         scrollers[i].style.backgroundPosition = "0px " + y + "px";
